d88093965d56de5b15532d71887f8cd3e403d0db
Onome Oviero <onomevmeruwoma@yahoo.com>
2022-06-06 03:02:23 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import Hero from './assets/images/image-header-mobile.jpg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container1">
+      <div className="container2">
+        
+        <div className="hero_section">
+          <img src={Hero} alt="header"/>
+        </div>
+        <div className="continuation">
+
+        <div className="content_section">
+          <h2>Get <strong>insights</strong> that help your business grow.</h2>
+          <h5>Discover the benefits of data analytics and make better decisions regarding revenue, customer experience, and overall efficiency.</h5>
+        </div>
+
+        
+        <div className="achievements">
+          <div className="companies">
+            <h1>10k+</h1>
+            <h4>companies</h4>
+          </div>
+
+          <div className="templates">
+            <h1>314</h1>
+            <h4>templates</h4>
+          </div>
+
+          <div className="quaries">
+            <h1>12M+</h1>
+            <h4>queries</h4>
+          </div>
+        </div>
+        </div>
+      </div>
     </div>
   );
 }
