59a21a8756fb76959e68bd99d8427b9775682c93
Onome Oviero <onomevmeruwoma@yahoo.com>
2022-06-14 12:25:10 +0100

    worked on the color and font-size

--- src/App.js
+++ src/App.js
@@ -13,24 +13,24 @@ function App() {
 
         <div className="content_section">
           <h2>Get <strong>insights</strong> that help your business grow.</h2>
-          <h5>Discover the benefits of data analytics and make better decisions regarding revenue, customer experience, and overall efficiency.</h5>
+          <h4>Discover the benefits of data analytics and make better decisions regarding revenue, customer experience, and overall efficiency.</h4>
         </div>
 
         
         <div className="achievements">
           <div className="companies">
-            <h1>10k+</h1>
-            <h4>companies</h4>
+            <h3>10k+</h3>
+            <h6>companies</h6>
           </div>
 
           <div className="templates">
-            <h1>314</h1>
-            <h4>templates</h4>
+            <h3>314</h3>
+            <h6>templates</h6>
           </div>
 
           <div className="quaries">
-            <h1>12M+</h1>
-            <h4>queries</h4>
+            <h3>12M+</h3>
+            <h6>queries</h6>
           </div>
         </div>
         </div>
